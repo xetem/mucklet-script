2f041af35643a2891c1ac11a9f9d539366272bf3
Xetem Ilekex <91496021+xetem@users.noreply.github.com>
2025-01-18 01:27:39 -0800

    selective entry script

---
+++ script/selective_entry.ts
@@ -0,0 +1,28 @@
+const exitId = "ccr2fcu9gbrk70si8020"
+
+export function onActivate(): void {
+    Room.listenExit(exitId)
+}
+
+export function onExitUse(addr: string, exitIntercept: ExitIntercept): void {
+    if(exitIntercept.exitId === exitId && !matchGender((Room.getChar(exitIntercept.charId) as Room.Char).gender as string)) {
+        exitIntercept.cancel("The door remains locked for those who do not drink of the elixer, or who are naturally a maleherm.")
+    } else {
+        exitIntercept.useExit()
+    }
+}
+
+function matchGender(gender: string): boolean {
+    gender = gender.trim()
+    gender = gender.toLowerCase()
+    gender = gender.replaceAll(".", "")
+    gender = gender.replaceAll(" ", "")
+    gender = gender.replaceAll("-", "")
+    return gender.includes("maleherm")
+        || gender.includes("mascherm")
+        || gender.includes("masculineherm")
+        || (gender.includes("mherm") && ! gender.includes("femherm") )
+        || gender.includes("boyherm")
+        || gender.includes("boiherm")
+        || gender.includes("hermandrodite")
+}
